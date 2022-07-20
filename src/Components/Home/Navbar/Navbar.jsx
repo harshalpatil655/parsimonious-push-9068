17c1de2175bd3f38566d86b9a7a6d1b3be76d247
harshalpatil655 <harshalpatil655@gmail.com>
2022-07-20 21:28:07 +0530

    added Navbar &grid

---
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Topbar } from "./Topbar";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/Shopping.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Shopping = () => {
+  return <div>Shopping</div>;
+};
